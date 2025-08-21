7c5406c36b29088fed5ded8d253856438d68d93d
Oğulcan Yenigül <ogulcanyenigul@gmail.com>
2025-08-21 18:45:54 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Daily Spent Money</h1>
+      <p>Hoş geldin! Uygulaman çalışıyor 🎉</p>
+    </div>
+  )
+}
+
+export default App
